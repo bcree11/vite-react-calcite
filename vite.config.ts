5484faf9e7ae842866d68a0817134b3251832505
Brandon Cree <bcree@esri.com>
2021-12-14 17:28:56 -0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-  build: {
-    rollupOptions: {
-      input: "src/main.tsx",
-      output: [{ dir: path.resolve('public'), format: 'es' }]
-    }
-  }
+  plugins: [react()]
 });
